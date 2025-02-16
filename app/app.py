36fbbd331407154d90530ca88b2aaa98b43a786e
Soeren Herrmann <33161332+PixelWelt@users.noreply.github.com>
2025-02-16 14:15:29 +0100

    Add docstrings for home route and get_library function

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import flask
 from flask import render_template
 from flask_cors import CORS
+
 import library_manager
 
 app = flask.Flask(__name__)
@@ -8,10 +9,13 @@ CORS(app)
 
 @app.route('/')
 def home():
+    """Home route that renders the index page with a list of games.
+
+    Returns:
+        flask.Response: The rendered template for the index page with the list of games.
+    """
     games = library_manager.get_library()
     print(games)
     return render_template('index.jinja2', games=games)
-
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
--- app/library_manager.py
+++ app/library_manager.py
@@ -1,9 +1,23 @@
-import os
 import json
+import os
+
 import loguru
 import yaml
 
+
 def get_library() -> json:
+    """Retrieve the list of games from the static directory.
+
+    This function scans the 'static/games' directory for subdirectories, each representing a game.
+    It reads the 'config.yaml' file in each subdirectory to gather game information and constructs
+    a list of game dictionaries.
+
+    Returns:
+        json: A list of dictionaries, each containing information about a game.
+
+    Raises:
+        FileNotFoundError: If the 'config.yaml' file is not found in a game's directory.
+    """
     games = []
     for dir in os.listdir('static/games'):
         if os.path.isdir('static/games/' + dir):
